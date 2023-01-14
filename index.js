f4c89e07d55a58c59003df6b51cdaba5fa056769
mimoku <122697117+mimoku@users.noreply.github.com>
2023-01-14 21:41:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const { Plugins } = require ("./src/index.js")
 
 
-console.log('\033[36mmidori.js is trying to be packed!');
+console.log('\033[36mmidori.js-lite is trying to be packed!');
 
 
-console.log('\033[92mDiscord server : https://discord.gg/xPURT2B3sA'); setTimeout(() => { 
+console.log('\033[92mDiscord Server : https://discord.gg/rSACavZdD5'); setTimeout(() => { 
   
-console.log('\033[32mmidori.js is now sucessfully packed!'); }, 1500);
+console.log('\033[32mMidori.js-lite is now sucessfully packed!'); }, 1500);
 
 
 module.exports = {
